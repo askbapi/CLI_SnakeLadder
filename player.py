4a7b96cbdd03f4933f6c1ed110e01d873a4e9ca5
bapi <mail2bapi@astrosoft.co.in>
2017-10-24 22:10:36 +0530

    Basic Player

---
+++ player.py
@@ -0,0 +1,11 @@
+# PLay will role the dii
+# Move the palyer through Board
+
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self.name
