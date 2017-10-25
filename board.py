1a132a32857385b9b320a91ec4e2d4ba8efb38b7
bapi <mail2bapi@astrosoft.co.in>
2017-10-26 03:48:59 +0530

    Board has player position to down now

--- board.py
+++ board.py
@@ -7,19 +7,19 @@ import random
 
 
 class Board:
-
     def __init__(self, rows=0, cols=0):
         self.rows = int(rows)
         self.cols = int(cols)
-        self.cells = list(range(1, (self.rows*self.cols)+1))
+        self.cells = {}
+        for cell_no in range(1, (self.rows * self.cols) + 1):
+            self.cells[cell_no] = ' [{:5d}] '.format(cell_no)
 
     def __str__(self):
         return 'Board has {} rows and {} columns.'.format(self.rows, self.cols)
 
 
 class SnakeLadderBoard(Board):
-
-    def __init__(self, rows=0, cols=0, snakes=0, ladder=0):
+    def __init__(self, rows=0, cols=0, snakes=0, ladder=0, **kwarg):
         super().__init__(rows=rows, cols=cols)
         self.snakes_head = []
         self.snakes_tail = []
@@ -29,7 +29,6 @@ class SnakeLadderBoard(Board):
         self.__set_ladders(ladder)
 
     def __set_snakes(self, snakes=0):
-
         while len(self.snakes_head) <= snakes:
             # Last cell cannot have  snake head
             max_range = len(self.cells) - 1
@@ -47,51 +46,70 @@ class SnakeLadderBoard(Board):
                     self.snakes_tail.append(snakes_tail)
 
     def __set_ladders(self, ladder=0):
-
         while len(self.ladder_head) <= ladder:
             # Last cell cannot have  ladder head
             max_range = len(self.cells) - 10
 
             # Snake head cannot be on first row
-            ladder_head = random.randrange(self.cols+1, max_range)
+            ladder_head = random.randrange(self.cols + 1, max_range)
             digit_list = list(str(ladder_head))
 
             # Tail should  not be in same row and should be above
             if int(ladder_head) > 10:
                 minimum_value = (int(digit_list[0]) * 10)
                 ladder_tail = random.randrange(1, minimum_value)
-                #if ladder_tail > minimum_value & ladder_head != ladder_tail:
+                # if ladder_tail > minimum_value & ladder_head != ladder_tail:
                 if ladder_head not in self.snakes_head:
                     self.ladder_head.append(ladder_head)
 
                 if ladder_tail not in self.snakes_tail:
                     self.ladder_tail.append(ladder_tail)
 
-    def draw(self):
-        col_draw = ''
+    def draw(self, pointers):
+        print('=' * 100)
+
         i = 1
-        self.cells.reverse()
-        print('=' * 5 * 15)
-        for cell in self.cells:
+        col_draw = ''
+        for cell, cell_value in self.cells.items():
+            # Adding Snake Head
             if cell in self.snakes_head:
-                col_draw += '[-<{:3d}]'.format(cell)
+                if cell in pointers.keys():
+                    self.cells[cell] = '[-<{:3d} {}]'.format(cell, pointers[cell])
+                else:
+                    self.cells[cell] = '[-<{:3d}]'.format(cell)
 
+            # Adding Snake Tail
             elif cell in self.snakes_tail:
-                col_draw += '[>-{:3d}]'.format(cell)
+                if cell in pointers.keys():
+                    self.cells[cell] = '[-<{:3d} {}]'.format(cell, pointers[cell])
+                else:
+                    self.cells[cell] = '[-<{:3d}]'.format(cell)
 
+            # Adding Ladder Head
             elif cell in self.ladder_head:
-                col_draw += '[H{:4d}]'.format(cell)
+                if cell in pointers.keys():
+                    self.cells[cell] = '[-<{:3d} {}]'.format(cell, pointers[cell])
+                else:
+                    self.cells[cell] = '[-<{:3d}]'.format(cell)
 
+            # Adding Ladder Tail
             elif cell in self.ladder_tail:
-                col_draw += '[H{:4d}]'.format(cell)
+                if cell in pointers.keys():
+                    self.cells[cell] = '[H{:4d} {}]'.format(cell, pointers[cell])
+                else:
+                    self.cells[cell] = '[H{:4d}]'.format(cell)
 
-            else:
-                col_draw += '[{:5d}]'.format(cell)
+            if cell in pointers.keys():
+                self.cells[cell] = '[-<{:3d} {}]'.format(cell, pointers[cell])
 
+            col_draw += self.cells[cell]
             if i >= 10:
                 i = 0
-                print(col_draw)
+                print("|    " + col_draw + "   |")
                 col_draw = ''
 
             i += 1
-        print('=' * 5 * 15)
+        print('=' * 100)
+
+
+
