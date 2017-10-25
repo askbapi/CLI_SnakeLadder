77f3c4d50f2c79572ba29c7d4dec5644dcc3325b
bapi <mail2bapi@astrosoft.co.in>
2017-10-26 01:37:05 +0530

    Added Player position

--- player.py
+++ player.py
@@ -5,7 +5,21 @@
 class Player:
     def __init__(self, name):
         self.name = name
+        self.current_position = 0
 
     @property
     def name(self):
         return self.name
+
+    @property.setter
+    def name(self, name):
+        self.name = name
+
+    @property
+    def current_position(self):
+        return self.current_position
+
+    @property.setter
+    def current_position(self, position):
+        self.current_position = position
+
